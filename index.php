ee420b3719d37f4fc94d80089c9f9755e9700652
Jeffrey Sambells <github@tropicalpixels.com>
2010-10-14 12:57:04 -0700

    Fixed a few validation errors.

--- index.php
+++ index.php
@@ -17,7 +17,12 @@
 					<nav>
 						<ul>
 							<li><?php comments_popup_link('Leave your comment', 'One comment', '% comments'); ?><?php the_tags(' &bull; Tagged as: ', ', ', ''); ?></li>
-							<li>Share on <a href="http://twitter.com/home?status=Currently reading: <?php the_title_attribute(); ?> <?php the_permalink(); ?>">Twitter</a>, <a href="http://www.facebook.com/share.php?u=<?php the_permalink(); ?>&amp;t=<?php the_title_attribute(); ?>">Facebook</a>, <a href="http://del.icio.us/post?v=4;url=<?php the_permalink(); ?>">Delicious</a>, <a href="http://digg.com/submit?url=<?php the_permalink(); ?>">Digg</a>, <a href="http://www.reddit.com/submit?url=<?php the_permalink(); ?>&amp;title=<?php the_title_attribute(); ?>">Reddit</a></li>
+							<li>Share on 
+							<a href="http://twitter.com/home?status=Reading:+<?php urlencode(the_permalink()); ?>">Twitter</a>,
+							<a href="http://www.facebook.com/share.php?u=<?php urlencode(the_permalink()); ?>">Facebook</a>,
+							<a href="http://del.icio.us/post?v=4;url=<?php urlencode(the_permalink()); ?>">Delicious</a>,
+							<a href="http://digg.com/submit?url=<?php urlencode(the_permalink()); ?>">Digg</a>,
+							<a href="http://www.reddit.com/submit?url=<?php urlencode(the_permalink()); ?>">Reddit</a></li>
 							<?php edit_post_link('Edit this post', '<li>', '</li>'); ?>
 						</ul>
 					</nav>
