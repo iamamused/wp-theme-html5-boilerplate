e09eb604169ab563c0918bdc043de4a77479dbc1
Jeffrey Sambells <github@tropicalpixels.com>
2011-09-19 21:35:16 -0700

    new style and under to html5bp 2

--- footer.php
+++ footer.php
@@ -6,22 +6,25 @@
   
   <!-- Javascript at the bottom for fast page loading -->
   
-    <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-    <script>!window.jQuery && document.write('<script src="<?php bloginfo('template_url'); ?>/js/jquery-1.4.2.min.js"><\/script>')</script>
-  
-  
-    <script src="<?php bloginfo('template_url'); ?>/js/plugins.js?v=1"></script>
-    <script src="<?php bloginfo('template_url'); ?>/js/script.js?v=1"></script>
-  
-    <!--[if lt IE 7 ]>
-      <script src="js/dd_belatedpng.js?v=1"></script>
-    <![endif]-->
-  
-  	<!-- wp_footer() content added by plugins -->
-  	<!-- Do not remove this, it's required for certain plugins which generally use this hook to reference JavaScript files. -->
-		
-	<?php wp_footer(); ?>
+  <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script>!window.jQuery && document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+
+
+  <script src="<?php bloginfo('template_url'); ?>/js/plugins.js?v=1"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/script.js?v=1"></script>
+
+  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+  <!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+
+  <!-- wp_footer() content added by plugins -->
+  <!-- Do not remove this, it's required for certain plugins which generally use this hook to reference JavaScript files. -->
+	
+  <?php wp_footer(); ?>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -27,8 +27,8 @@ Jeffrey
 <head>
   <meta charset="utf-8">
 
-  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
-       Remove this if you use the .htaccess -->
+  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
+       More info: h5bp.com/b/378 -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
@@ -38,29 +38,37 @@ Jeffrey
   <meta name="author" content="">
 
   <!--  Mobile viewport optimized: j.mp/bplateviewport -->
-  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
 
-  <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
   <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.png">
   <link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/apple-touch-icon.png">
 
 
   <!-- CSS : implied media="all" -->
+  <!-- CSS concatenated and minified via ant build script-->
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v=1">
+  
+  <!-- Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=IM+Fell+DW+Pica+SC' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Neuton:300,400,700,400italic' rel='stylesheet' type='text/css'>
+  
+  <link  href="http://fonts.googleapis.com/css?family=Damion:regular" rel="stylesheet" type="text/css" >
+  <link  href="http://fonts.googleapis.com/css?family=Anton:regular" rel="stylesheet" type="text/css" >
 
+  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
  
-  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
-  <script src="<?php bloginfo('template_url'); ?>/js/modernizr-1.5.min.js"></script>
+  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
+     Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
+     For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
+  <script src="<?php bloginfo('template_url'); ?>/js/libs/modernizr-2.0.6.min.js"></script>
 
-
-  <!-- Pingback url -->
+  <!-- WordPress Pingback url -->
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-  <!-- wp_head() content added by plugins etc. -->	
+  <!-- wp_head() content added by plugins etc. This isn't ideal but it will work. -->	
   <?php wp_head(); ?>
-
-	<link  href="http://fonts.googleapis.com/css?family=Damion:regular" rel="stylesheet" type="text/css" >
-	<link  href="http://fonts.googleapis.com/css?family=Anton:regular" rel="stylesheet" type="text/css" >
   
 </head>
 
@@ -69,19 +77,9 @@ Jeffrey
 
   <div id="container">
     <header id="header">
-    	<a href="/"><img id="bookmark" src="<?php /* echo get_option('bookmark_image_url') */ ?><?php bloginfo('template_url'); ?>/images/bookmark.png" alt="<?php bloginfo('name'); ?> Bookmark"></a>
-    
-		<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-
-		<nav>
-			<?php wp_nav_menu( array( 
-				'theme_location' => 'menu-1',
-				'sort_column' => 'menu_order',
-				'container_class' => 'menu-header', 
-			) ); ?>
-		</nav>
-		
+    	<a href="/"><img id="bookmark" src="<?php /* echo get_option('bookmark_image_url') */ ?><?php bloginfo('template_url'); ?>/images/bookmark.png" alt="<?php bloginfo('name');?>">
+    		<nav><a href="">More</a></nav>
     </header>
-    
+    	
     <div id="main">
-    
+    	
